549b0e2d03fb5659829aa225a00d8765cb7e4635
Jason Vandeyar <jasonvandeyar55@gmail.com>
2023-05-24 05:03:08 +0200

    api request setup for Sheety and post request made for exercises done

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import requests
+from datetime import datetime
 
 APP_ID = "1b931bd5"
 API_KEY = "2c4d00aab20f4cb2d9e470f27d1a83e2"
+USERNAME = "6c0887c27fe16c2892977f767bcbbd82"
 
 exercises_done = input("Tell me which exercises you did: ")
+current_datetime = datetime.now()
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -18,4 +21,20 @@ exercise_params = {
 }
 
 res = requests.post(url=exercise_endpoint, json=exercise_params, headers=headers)
-print(res.text)
+exercise_data = res.json()
+
+sheety_endpoint = f"https://api.sheety.co/{USERNAME}/workoutTracking/workouts"
+
+for exercise in exercise_data['exercises']:
+    sheety_params = {
+        "workout": {
+            "date": current_datetime.strftime("%d/%m/%Y"),
+            "time": current_datetime.strftime("%H:%M:%S"),
+            "exercise": exercise['user_input'],
+            "duration": exercise['duration_min'],
+            "calories": exercise['nf_calories']
+        }
+    }
+
+    exercise_res = requests.post(url=sheety_endpoint, json=sheety_params)
+    print(exercise_res.text)
